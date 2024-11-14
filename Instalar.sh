e290f351c76770dc8cfa6f38cdf614a450e2873b
Woods <162640861+TatoGar12@users.noreply.github.com>
2024-11-14 19:07:49 -0300

    Update Instalar.sh

--- Instalar.sh
+++ Instalar.sh
@@ -1,3 +1,1 @@
-sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-sudo chmod +x /usr/local/bin/docker-compose
-docker-compose --version
+https://prod.liveshare.vsengsaas.visualstudio.com/join?6C425E910ABB67C0FDBA765F3DF904232C93
